f6b78830f5a8d5e60843957a055008ad8ff5570b
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2022-12-06 00:47:15 +0200

    added validateForm

--- js/select.js
+++ js/select.js
@@ -1,51 +1,8 @@
-const refs = {
-  input: document.querySelector("#phone"),
-  inputName: document.querySelector("#name"),
-  error: document.querySelector(".alert-error"),
-  info: document.querySelector(".alert-info"),
-  modal: document.querySelector("[data-modal]"),
-};
+const input = document.querySelector("#phone");
 
-const phoneInput = window.intlTelInput(refs.input, {
+const phoneInput = window.intlTelInput(input, {
   preventInvalidNumbers: true,
   separateDialCode: true,
   excludeCountries: ["ru", "by"],
   preferredCountries: ["ua", "pl", "gb", "us"],
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
-
-refs.inputName.addEventListener("input", emptyInput);
-
-function process(event) {
-  event.preventDefault();
-
-  const phoneNumber = phoneInput.getNumber();
-
-  refs.error.style.display = "none";
-  //   refs.info.style.display = "none";
-
-  if (phoneInput.isValidNumber()) {
-    refs.input.style.borderColor = "green";
-    // refs.modal.classList.toggle("is-hidden");
-    return true;
-  } else {
-    refs.error.style.display = "";
-    refs.error.classList.add("visual");
-    refs.error.innerHTML = `Invalid phone number.`;
-    refs.input.style.borderColor = "red";
-    return false;
-  }
-}
-
-function emptyInput(event) {
-  if (refs.inputName.value === "") {
-    refs.info.style.display = "";
-    refs.info.classList.add("visual");
-    refs.info.innerHTML = `This line is empty.`;
-    refs.inputName.style.borderColor = "red";
-  } else {
-    refs.info.innerHTML = "";
-    refs.inputName.style.borderColor = "green";
-  }
-}
